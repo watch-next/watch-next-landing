0d3afaaad7a6f4190f0b961013be5e38a9249eb6
Rafael Pádua <145411939+rafaelPadua001@users.noreply.github.com>
2026-07-12 00:42:02 -0300

    fix

--- api/tmdb/[...path].ts
+++ api/tmdb/[...path].ts
@@ -19,7 +19,7 @@ import {
   proxyToTmdb,
   extractPathFromQuery,
   filterQueryParams,
-} from '../../server/tmdb/proxyHandler';
+} from '../../server/tmdb/proxyHandler.js';
 
 const TMDB_API_BASE =
   process.env.TMDB_API_BASE || 'https://api.themoviedb.org/3';
